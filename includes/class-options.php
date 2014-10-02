572d5508da713ff9dd4ec673f603ae0b8007bc35
Caroline Geven <caroline@yoast.com>
2014-10-02 14:24:24 +0200

    Added SVG Icon for menu item

--- includes/class-options.php
+++ includes/class-options.php
@@ -106,59 +106,9 @@ if ( ! class_exists( 'Yoast_Sociable_Options' ) ) {
 
         /**
          * Upgrade the settings when settings are changed.
-         *
-         * @since 5.0.1
          */
         private function upgrade() {
-            if ( ! isset( $this->options['version'] ) && is_null( $this->get_tracking_code() ) ) {
-                $old_options = get_option( 'Yoast_Google_Analytics' );
 
-                if ( isset( $old_options ) && is_array( $old_options ) ) {
-                    if ( isset( $old_options['uastring'] ) && '' !== trim( $old_options['uastring'] ) ) {
-                        // Save UA as manual UA, instead of saving all the old GA crap
-                        $this->options['manual_ua_code']       = 1;
-                        $this->options['manual_ua_code_field'] = $old_options['uastring'];
-                    }
-
-                    // Other settings
-                    $this->options['allow_anchor']               = $old_options['allowanchor'];
-                    $this->options['add_allow_linker']           = $old_options['allowlinker'];
-                    $this->options['anonymous_data']             = $old_options['anonymizeip'];
-                    $this->options['track_outbound']             = $old_options['trackoutbound'];
-                    $this->options['track_internal_as_outbound'] = $old_options['internallink'];
-                    $this->options['track_internal_as_label']    = $old_options['internallinklabel'];
-                    $this->options['extensions_of_files']        = $old_options['dlextensions'];
-
-                }
-
-                delete_option( 'Yoast_Google_Analytics' );
-            }
-
-            // 5.0.0 to 5.0.1 fix of ignore users array
-            if ( ! isset( $this->options['version'] ) || version_compare( $this->options['version'], '5.0.1', '<' ) ) {
-                if ( isset( $this->options['ignore_users'] ) && ! is_array( $this->options['ignore_users'] ) ) {
-                    $this->options['ignore_users'] = (array) $this->options['ignore_users'];
-                }
-            }
-
-            // Check is API option already exists - if not add it
-            $yst_ga_api = get_option( 'yst_ga_api' );
-            if ( $yst_ga_api === false ) {
-                add_option( 'yst_ga_api', array(), '', 'no' );
-            }
-
-            // Fallback to make sure every default option has a value
-            $defaults = $this->default_sociable_values();
-            foreach ( $defaults[$this->option_prefix] as $key => $value ) {
-                if ( ! isset( $this->options[$key] ) ) {
-                    $this->options[$key] = $value;
-                }
-            }
-
-            // Set to the current version now that we've done all needed upgrades
-            $this->options['version'] = GAWP_VERSION;
-
-            $this->update_option( $this->options );
         }
 
         /**
