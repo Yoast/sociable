0b7cbb09b98838c3db0fd94ad3274c34d11aec56
Caroline Geven <caroline@yoast.com>
2014-10-13 11:55:20 +0200

    Removed get_social_networks function as it is now in Yoast_Sociable class

--- includes/class-options.php
+++ includes/class-options.php
@@ -80,34 +80,6 @@ if ( ! class_exists( 'Yoast_Sociable_Options' ) ) {
         }
 
         /**
-         * Return all active social networks
-         *
-         * @return array
-         */
-        public function get_social_networks() {
-            $social_networks = array();
-
-            $this->options = $this->get_options();
-
-            if ( ! empty( $this->options['networks'] ) ) {
-                $networks = $this->options['networks'];
-
-                $social_networks = explode ( ',', $networks );
-
-                foreach ($social_networks as $position => $social_network) {
-                    //For now, only get name
-                    $social_networks[$position] = $social_network;
-
-                    //To do: call social network class for each network and get link and SVG icon from class - add this to array
-
-                }
-
-            }
-
-            return $social_networks;
-        }
-
-        /**
          * Check if all the options are set, to prevent a notice if debugging is enabled
          * When we have new changes, the settings are saved to the options class
          *
