e454f3e326835b51792c9aed5585407435a29af4
Caroline Geven <caroline@yoast.com>
2014-10-16 16:57:33 +0200

    Added function to abstract class

--- includes/class-sociable-social-button.php
+++ includes/class-sociable-social-button.php
@@ -1,9 +1,15 @@
 <?php
 
-abstract class Sociable_Social_Button {
+abstract class Yoast_Sociable_Social_Button {
 
+	/**
+	 * @return String
+	 */
 	abstract protected function getSVG();
 
-}
+	/**
+	 * @return String
+	 */
+	abstract protected function getLink();
 
-?>
+}
